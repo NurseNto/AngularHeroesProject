4eeb294855560bcbde3a14786c9dce03b6bd428f
DA MAC 29 <damac29@DS-LEARNERs-MacBook-Air-29.local>
2023-04-24 15:29:46 +0200

    load heroes from web api and can use crud

--- angular-tour-of-heroes/src/app/hero-detail/hero-detail.component.ts
+++ angular-tour-of-heroes/src/app/hero-detail/hero-detail.component.ts
@@ -1,18 +1,17 @@
-import { Component, Input } from '@angular/core';
-import { Hero } from '../hero';
-
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 
+import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-hero-detail',
   templateUrl: './hero-detail.component.html',
-  styleUrls: ['./hero-detail.component.scss']
+  styleUrls: [ './hero-detail.component.scss' ]
 })
-export class HeroDetailComponent {
-  @Input() hero?: Hero;
+export class HeroDetailComponent implements OnInit {
+  hero: Hero | undefined;
 
   constructor(
     private route: ActivatedRoute,
@@ -20,18 +19,24 @@ export class HeroDetailComponent {
     private location: Location
   ) {}
 
-
   ngOnInit(): void {
     this.getHero();
   }
-  
+
   getHero(): void {
-    const id = Number(this.route.snapshot.paramMap.get('id'));
+    const id = parseInt(this.route.snapshot.paramMap.get('id')!, 10);
     this.heroService.getHero(id)
       .subscribe(hero => this.hero = hero);
   }
 
   goBack(): void {
     this.location.back();
+  }
+
+  save(): void {
+    if (this.hero) {
+      this.heroService.updateHero(this.hero)
+        .subscribe(() => this.goBack());
+    }
   }
 }
